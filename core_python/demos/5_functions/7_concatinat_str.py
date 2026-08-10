a31848ecce43bcc1fcd31aaf9a4b969db0dfd08c
rohit_patil_ai <rohitpa654321@gmail.com>
2026-08-10 17:36:25 +0530

    Added missing concept program...

---
+++ core_python/demos/5_functions/7_concatinat_str.py
@@ -0,0 +1,19 @@
+### WAP to concatinate the string...
+
+def concatiStr(a,b):
+    return a+b
+
+print(concatiStr(input('Enter String : '), input('Enter String :')))
+
+
+
+
+### WAP to concatinate the list...
+
+def concatiStr(a,b):
+    return a+b
+
+
+a = [1,3,4,5,6,7,7,8]
+b = [1,3,4,5,6,7,7,8]
+print(concatiStr(a,b))
